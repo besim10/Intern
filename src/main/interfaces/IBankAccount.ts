6f28503a1df54e05399761c04e2282de99be37e9
besim10 <besimsokoli10@gmail.com>
2022-04-29 17:59:28 +0200

    created shipping address page and payment page

---
+++ src/main/interfaces/IBankAccount.ts
@@ -0,0 +1,10 @@
+export default interface IBankAccount
+{
+	id: number;
+    code: string;
+    name: string;
+    currencyId: number;
+    balance: number,
+    clientId: number;
+    isActive: boolean
+}
